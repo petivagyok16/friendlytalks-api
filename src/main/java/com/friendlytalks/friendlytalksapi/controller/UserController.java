5a2bedb8db8b6f742fef3f19e10b57391ab9899b
Peter P <peter.palocz91@gmail.com>
2018-03-20 13:49:35 +0100

    feat(UserController): add signin

--- src/main/java/com/friendlytalks/friendlytalksapi/controller/UserController.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.friendlytalks.friendlytalksapi.controller;
 
+import com.friendlytalks.friendlytalksapi.model.Credentials;
 import com.friendlytalks.friendlytalksapi.model.User;
 import com.friendlytalks.friendlytalksapi.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -27,11 +28,22 @@ public class UserController {
 	}
 
 	@RequestMapping(
+					value = "/signup",
 					method = RequestMethod.POST,
 					produces = MediaType.APPLICATION_JSON_VALUE
 	)
 	@ResponseStatus(HttpStatus.CREATED)
 	public void signUp(@RequestBody User user) {
 		this.userService.signUp(user);
+	}
+
+	@RequestMapping(
+					value = "/signin",
+					method = RequestMethod.POST,
+					produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	@ResponseStatus(HttpStatus.OK)
+	public void signIn(@RequestBody Credentials credentials) {
+		this.userService.signIn(credentials);
 	}
 }
