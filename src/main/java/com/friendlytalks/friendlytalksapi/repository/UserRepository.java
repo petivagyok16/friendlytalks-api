3daad3315c09fba171c738b4bd8e5f5b47487006
Peter P <peter.palocz91@gmail.com>
2018-03-20 13:50:47 +0100

    feat(UserRepo): add findByUsername method

--- src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.friendlytalks.friendlytalksapi.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository("userRepository")
 public interface UserRepository extends MongoRepository<User, String> {
+
+	Optional <User> findByUsername(String username);
 
 }
