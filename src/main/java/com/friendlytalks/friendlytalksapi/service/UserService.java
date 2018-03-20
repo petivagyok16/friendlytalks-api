4d709732ac7aeafef00d634567b6e51c0b5db037
Peter P <peter.palocz91@gmail.com>
2018-03-20 13:51:11 +0100

    feat(UserService): implement signin

--- src/main/java/com/friendlytalks/friendlytalksapi/service/UserService.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/service/UserService.java
@@ -1,14 +1,22 @@
 package com.friendlytalks.friendlytalksapi.service;
 
+import com.friendlytalks.friendlytalksapi.exceptions.WrongCredentialsException;
+import com.friendlytalks.friendlytalksapi.model.Credentials;
 import com.friendlytalks.friendlytalksapi.model.User;
 import com.friendlytalks.friendlytalksapi.repository.UserRepository;
+import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service("userService")
 public class UserService {
+
+//	final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
 	@Autowired private UserRepository userRepository;
 
 	public List<User> getAllUser() {
@@ -16,6 +24,21 @@ public class UserService {
 	}
 
 	public void signUp(User user) {
+		user.setPassword(passwordEncryptor.encryptPassword(user.getPassword()));
 		this.userRepository.insert(user);
+	}
+
+	public User signIn(Credentials credentials) {
+		Optional<User> user = this.userRepository.findByUsername(credentials.getUsername());
+
+		if (user.isPresent() && this.checkPassword(credentials.getPassword(), user.get().getPassword())) {
+			return user.get(); // TODO: don't send back the entire user!
+		} else {
+			throw new WrongCredentialsException("Wrong username or password!");
+		}
+	}
+
+	private boolean checkPassword(String inputPassword, String encryptedPassword) {
+		return this.passwordEncryptor.checkPassword(inputPassword, encryptedPassword);
 	}
 }
