60dd00f02d8dba0077859046b7e9cde5368faf6e
Peter P <peter.palocz91@gmail.com>
2018-03-21 16:48:10 +0100

    refactor(UserRepo): refactor indentation

--- src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository("userRepository")
 public interface UserRepository extends MongoRepository<User, String> {
 
-	Optional <User> findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
 }
