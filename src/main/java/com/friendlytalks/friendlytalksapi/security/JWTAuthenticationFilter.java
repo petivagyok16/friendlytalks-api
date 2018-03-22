c66e051356cb57a956efaef159a2d525fe492e2e
Peter P <peter.palocz91@gmail.com>
2018-03-22 17:00:35 +0100

    feat(Security): implement JWT authorization and authentication

--- src/main/java/com/friendlytalks/friendlytalksapi/security/JWTAuthenticationFilter.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/security/JWTAuthenticationFilter.java
@@ -1,26 +1,66 @@
 package com.friendlytalks.friendlytalksapi.security;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class JWTAuthenticationFilter extends GenericFilterBean {
+
+import static com.friendlytalks.friendlytalksapi.security.SecurityConstants.EXPIRATION_TIME;
+import static com.friendlytalks.friendlytalksapi.security.SecurityConstants.SECRET;
+import static com.friendlytalks.friendlytalksapi.security.SecurityConstants.HEADER_STRING;
+import static com.friendlytalks.friendlytalksapi.security.SecurityConstants.TOKEN_PREFIX;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	private AuthenticationManager authenticationManager;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+		setFilterProcessesUrl("/api/v1/signin");
+	}
 
 	@Override
-	public void doFilter(
-					ServletRequest request,
-					ServletResponse response,
-					FilterChain filterChain
-	) throws IOException, ServletException {
-		Authentication authentication = TokenAuthenticationService.getAuthentication((HttpServletRequest)request);
+	public Authentication attemptAuthentication(HttpServletRequest req,
+																							HttpServletResponse res) throws AuthenticationException {
+		try {
+			com.friendlytalks.friendlytalksapi.model.User credentials = new ObjectMapper()
+							.readValue(req.getInputStream(), com.friendlytalks.friendlytalksapi.model.User.class);
 
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		filterChain.doFilter(request,response);
+			return authenticationManager.authenticate(
+							new UsernamePasswordAuthenticationToken(
+											credentials.getUsername(),
+											credentials.getPassword(),
+											new ArrayList<>())
+			);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req,
+																					HttpServletResponse res,
+																					FilterChain chain,
+																					Authentication auth) throws IOException, ServletException {
+
+		String token = Jwts.builder()
+						.setSubject(((User) auth.getPrincipal()).getUsername())
+						.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+						.signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
+						.compact();
+		res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
 	}
 }
