10d5eafbcff16e3323052a4a22cc278f850d1617
Peter P <peter.palocz91@gmail.com>
2018-03-22 16:59:56 +0100

    refactor(WebSecurityConfig): simplify config

--- src/main/java/com/friendlytalks/friendlytalksapi/security/WebSecurityConfig.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/security/WebSecurityConfig.java
@@ -1,40 +1,14 @@
 package com.friendlytalks.friendlytalksapi.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-//						.antMatchers("/api/v1/users").permitAll()
-						.antMatchers(HttpMethod.POST, "/api/v1/users/signup").permitAll()
-						.antMatchers(HttpMethod.POST, "/api/v1/users/signin").permitAll()
-						.anyRequest().authenticated()
-						.and()
-						// We filter the api/login requests
-						.addFilterBefore(new JWTLoginFilter("/api/v1/users/signin", authenticationManager()),
-										UsernamePasswordAuthenticationFilter.class)
-						// And filter other requests to check the presence of JWT in header
-						.addFilterBefore(new JWTAuthenticationFilter(),
-										UsernamePasswordAuthenticationFilter.class);
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		 Create a default account
-		auth.inMemoryAuthentication()
-						.withUser("admin")
-						.password("{noop}password")
-						.roles("ADMIN");
-
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
