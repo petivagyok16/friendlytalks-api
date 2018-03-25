55e562d1b6c5e20a78317ccb6a83be452ffc229a
peter palocz <peter.palocz91@gmail.com>
2018-03-25 18:31:54 +0200

    refactor(UserController): delete imports & refactor method

--- src/main/java/com/friendlytalks/friendlytalksapi/controller/UserController.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.friendlytalks.friendlytalksapi.controller;
 
-import com.friendlytalks.friendlytalksapi.model.Credentials;
 import com.friendlytalks.friendlytalksapi.model.User;
 import com.friendlytalks.friendlytalksapi.service.AuthenticationService;
 import com.friendlytalks.friendlytalksapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +20,7 @@ public class UserController {
 					method = RequestMethod.GET,
 					produces = MediaType.APPLICATION_JSON_VALUE
 	)
-	public List<User> getAllStudents() {
+	public List<User> getAllUser() {
 		return this.userService.getAllUser();
 	}
 }
