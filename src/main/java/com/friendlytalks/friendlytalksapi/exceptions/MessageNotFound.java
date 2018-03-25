c8e650a3a0b1ea245969cc85bb7cc83dbed6eda2
peter palocz <peter.palocz91@gmail.com>
2018-03-25 19:20:55 +0200

    feat(MessageException): implement exception

---
+++ src/main/java/com/friendlytalks/friendlytalksapi/exceptions/MessageNotFound.java
@@ -0,0 +1,7 @@
+package com.friendlytalks.friendlytalksapi.exceptions;
+
+public class MessageNotFound extends RuntimeException {
+	public MessageNotFound(String message) {
+		super(message);
+	}
+}
