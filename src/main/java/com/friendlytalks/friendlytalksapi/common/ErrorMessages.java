ef5dbacf03fa11d1dff7e0f4b551e86dcb1d7442
peter palocz <peter.palocz91@gmail.com>
2018-03-25 14:31:01 +0200

    fix(ErrorMessages): fix typo

--- src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
@@ -2,7 +2,7 @@ package com.friendlytalks.friendlytalksapi.common;
 
 public class ErrorMessages {
 	public static final String USER_NOT_FOUND= "User not found!";
-	public static final String USER_ALREADY_EXISTS= "User already exist";
+	public static final String USER_ALREADY_EXISTS= "User already exists";
 	public static final String WRONG_CREDENTIALS= "Wrong username or password!";
 	public static final String INVALID_TOKEN= "Invalid authentication token!";
 }
