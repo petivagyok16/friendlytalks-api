f46347138419b95304e6aa07f8163287e14f8b38
peter palocz <peter.palocz91@gmail.com>
2018-04-02 18:36:14 +0200

    refactor(WebSecurity): refactor to constructor DI

--- src/main/java/com/friendlytalks/friendlytalksapi/security/WebSecurity.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/security/WebSecurity.java
@@ -3,34 +3,32 @@ package com.friendlytalks.friendlytalksapi.security;
 import com.friendlytalks.friendlytalksapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Bean;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
 import static com.friendlytalks.friendlytalksapi.security.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-	@Autowired private UserDetailsService userDetailsService;
-	@Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
-	@Autowired private UserRepository userRepository;
+	private UserDetailsService userDetailsService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private UserRepository userRepository;
+
+	@Autowired
+	public WebSecurity(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.userRepository = userRepository;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
