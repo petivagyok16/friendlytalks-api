96bfca2b757cc2804a8b811b131181d415bccf8a
Peter P <peter.palocz91@gmail.com>
2018-04-09 16:58:36 +0200

    feat(InvalidTokenException): add response status

--- src/main/java/com/friendlytalks/friendlytalksapi/exceptions/InvalidTokenException.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/exceptions/InvalidTokenException.java
@@ -1,5 +1,9 @@
 package com.friendlytalks.friendlytalksapi.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class InvalidTokenException extends RuntimeException {
 	public InvalidTokenException(String message) {
 		super(message);
