b8cc7999b7e019b4277878538dfe140c85fb81a9
Peter P <peter.palocz91@gmail.com>
2018-04-24 17:39:44 +0200

    ref: remove service name

--- src/main/java/com/friendlytalks/friendlytalksapi/service/UserService.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-@Service("userService")
+@Service
 public class UserService {
 
 	private final UserRepository userRepository;
