1d6e5f8a9b02f6a316a54414af6cbd506174d715
Peter P <peter.palocz91@gmail.com>
2018-04-24 18:09:06 +0200

    refactor(Swagger): remove swagger from project temporary

--- src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
@@ -2,10 +2,8 @@ package com.friendlytalks.friendlytalksapi.repository;
 
 import com.friendlytalks.friendlytalksapi.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
 
 	Mono<User> findByUsername(String username);
