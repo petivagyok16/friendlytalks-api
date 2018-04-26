b2ee6e1cc7e06200168eb5a2fc5f810c9fe12969
Peter P <peter.palocz91@gmail.com>
2018-04-26 16:17:47 +0200

    feat(ReactiveWebSecurity): implement reactive web security

--- src/main/java/com/friendlytalks/friendlytalksapi/security/ReactiveWebSecurity.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/security/ReactiveWebSecurity.java
@@ -1,31 +1,48 @@
 package com.friendlytalks.friendlytalksapi.security;
 
 import com.friendlytalks.friendlytalksapi.repository.UserRepository;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+import org.springframework.util.Assert;
 
+@Configuration
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class ReactiveWebSecurity {
+	private ServerAuthenticationEntryPoint entryPoint = new JwtAuthenticationEntryPoint();
 
-	private ReactiveUserDetailsService reactiveUserDetailsService;
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	private UserRepository userRepository;
+	private final ReactiveUserDetailsService reactiveUserDetailsService;
+	private final CustomAuthenticationConverter customAuthenticationConverter;
+
 
 	@Autowired
-	public ReactiveWebSecurity(ReactiveUserDetailsService reactiveUserDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+	public ReactiveWebSecurity(
+					ReactiveUserDetailsService reactiveUserDetailsService,
+					UserRepository userRepository,
+					CustomAuthenticationConverter customAuthenticationConverter
+	) {
+		Assert.notNull(reactiveUserDetailsService, "userDetailsService cannot be null");
+		Assert.notNull(customAuthenticationConverter, "customAuthenticationConverter cannot be null");
 		this.reactiveUserDetailsService = reactiveUserDetailsService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-		this.userRepository = userRepository;
+		this.customAuthenticationConverter = customAuthenticationConverter;
+
 	}
 
 	/**
@@ -41,56 +58,71 @@ public class ReactiveWebSecurity {
 	 */
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-		return http
-						.addFilterAt(basicToJwtAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
-						.addFilterAt(jwtAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
-						.authorizeExchange().pathMatchers(HttpMethod.GET, "/api/**").permitAll()
-						.and()
-						.authorizeExchange()
-						.pathMatchers(HttpMethod.POST, SecurityConstants.SIGN_IN_URL)
-						.authenticated()
-						.pathMatchers(SecurityConstants.API_ROUTE)
-						.authenticated()
-						.and()
-						.authorizeExchange()
-						.anyExchange()
-						.denyAll()
-						.and()
-						.csrf().disable()
-						.build();
+
+				// Disable default security.
+				http.httpBasic().disable();
+				http.formLogin().disable();
+				http.csrf().disable();
+				http.logout().disable();
+
+				// Add custom security.
+				http.authenticationManager(authenticationManager());
+
+				// Disable authentication for `/resources/**` routes.
+				http.authorizeExchange().pathMatchers("/resources/**").permitAll();
+				http.authorizeExchange().pathMatchers("/webjars/**").permitAll();
+
+				//Disable authentication for `/test/**` routes.
+				http.authorizeExchange().pathMatchers("/test/**").permitAll();
+
+				// Disable authentication for `/auth/**` routes.
+				http.authorizeExchange().pathMatchers("/auth/**").permitAll();
+
+				http.securityContextRepository(securityContextRepository());
+
+				http.authorizeExchange().anyExchange().authenticated();
+				//.and().httpBasic().disable();
+
+				http.addFilterAt(apiAuthenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
+				// .httpBasic().disable().csrf().disable();
+
+				return http.build();
 	}
 
-	/**
-	 * An {@link AuthenticationWebFilter} which authenticates the user with Http Basic authentication
-	 * and issues a token afterwards.
-	 *
-	 * @return basic to JWT AuthenticationWebFilter
-	 */
-	@Bean
-	protected AuthenticationWebFilter basicToJwtAuthenticationFilter() {
-		AuthenticationWebFilter webFilter =
-						new AuthenticationWebFilter(new UserDetailsRepositoryReactiveAuthenticationManager(this.reactiveUserDetailsService));
-		webFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers(HttpMethod.POST, SecurityConstants.SIGN_IN_URL));
-		webFilter.setAuthenticationSuccessHandler(
-						new WebFilterChainServerJWTAuthenticationSuccessHandler(
-								SecurityConstants.SECRET,
-								SecurityConstants.EXPIRATION_TIME,
-								SecurityConstants.ISSUER
-		));
-		return webFilter;
+	private ReactiveAuthenticationManager authenticationManager() {
+		CustomReactiveAuthenticationManager customReactiveAuthenticationManager =  new CustomReactiveAuthenticationManager(this.reactiveUserDetailsService);
+		return customReactiveAuthenticationManager;
 	}
 
-	/**
-	 * An {@link AuthenticationWebFilter} which validates the user's Authorization token
-	 * and gives access if the validation was successful.
-	 *
-	 * @return JWT AuthenticationWebFilter
-	 */
+	private AuthenticationWebFilter apiAuthenticationWebFilter() {
+		try {
+			AuthenticationWebFilter apiAuthenticationWebFilter = new AuthenticationWebFilter(authenticationManager());
+			apiAuthenticationWebFilter.setAuthenticationFailureHandler(new ServerAuthenticationEntryPointFailureHandler(this.entryPoint));
+			apiAuthenticationWebFilter.setAuthenticationConverter(this.customAuthenticationConverter);
+			apiAuthenticationWebFilter.setRequiresAuthenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/api/**"));
+
+			// Setting the Context Repo helped, not sure if I need this
+			apiAuthenticationWebFilter.setSecurityContextRepository(securityContextRepository());
+
+			return apiAuthenticationWebFilter;
+		} catch (Exception e) {
+			throw new BeanInitializationException("Could not initialize AuthenticationWebFilter apiAuthenticationWebFilter.", e);
+		}
+	}
+
 	@Bean
-	protected AuthenticationWebFilter jwtAuthenticationFilter() {
-		AuthenticationWebFilter webFilter = new AuthenticationWebFilter(new JWTReactiveAuthenticationManager());
-		webFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers(SecurityConstants.API_ROUTE));
-		webFilter.setAuthenticationConverter(new ServerHttpBearerAuthenticationConverter(SecurityConstants.SECRET));
-		return webFilter;
+	public WebSessionServerSecurityContextRepository securityContextRepository() {
+		return new WebSessionServerSecurityContextRepository();
+	}
+
+	@Bean
+	@Primary
+	public PasswordEncoder encoder() {
+		return new CustomPasswordEncoder();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
