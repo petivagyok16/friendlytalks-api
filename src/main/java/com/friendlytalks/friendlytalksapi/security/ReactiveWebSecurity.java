f64727c97d9dae32877f294e4865d3a475893ee0
Peter P <peter.palocz91@gmail.com>
2018-04-26 10:31:18 +0200

    refactor(WebSecurity): put WebSecurityChain into its own file

---
+++ src/main/java/com/friendlytalks/friendlytalksapi/security/ReactiveWebSecurity.java
@@ -0,0 +1,117 @@
+package com.friendlytalks.friendlytalksapi.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+
+@EnableWebFluxSecurity
+public class ReactiveWebSecurity {
+
+	private static final String LOGIN_ROUTE = "/api/v1/message/login";
+	private static final String API_ROUTE = "/api/**";
+
+	@Value("${DEFAULT_SECRET}")
+	private String secret;
+
+	@Value("${jwt.expiration_time}")
+	private long expirationTime;
+
+	@Value("${ISSUER}")
+	private String issuer;
+
+	/**
+	 * A custom UserDetailsService to provide quick user rights for Spring Security,
+	 * more formal implementations may be added as separated files and annotated as
+	 * a Spring stereotype.
+	 *
+	 * @return MapReactiveUserDetailsService an InMemory implementation of user details
+	 */
+	@Bean
+	private MapReactiveUserDetailsService userDetailsRepository() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+						.username("user")
+						.password("user")
+						.roles("USER", "ADMIN")
+						.build();
+		return new MapReactiveUserDetailsService(user);
+	}
+
+	/**
+	 * For Spring Security webflux, a chain of filters will provide user authentication
+	 * and authorization, we add custom filters to enable JWT token approach.
+	 * This chain will authenticate with Basic HTTP authentication on the LOGIN_ROUTE path and
+	 * authenticate with Bearer token authentication on the API_ROUTE path.
+	 *
+	 * @param http An initial object to build common filter scenarios.
+	 *             Customized filters are added here.
+	 * @return SecurityWebFilterChain A filter chain for web exchanges that will
+	 * provide security
+	 */
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		return http
+//						.httpBasic().disable()
+//						.formLogin().disable()
+//						.csrf().disable()
+//						.logout().disable()
+//						.authorizeExchange().pathMatchers(HttpMethod.GET, "/api/**").permitAll()
+//						.anyExchange().authenticated()
+//						.and()
+//						.build();
+						.authorizeExchange().pathMatchers(HttpMethod.GET, "/api/**").permitAll()
+						.and()
+						.addFilterAt(basicToJwtAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
+						.addFilterAt(jwtAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
+						.authorizeExchange()
+						.pathMatchers(HttpMethod.POST, LOGIN_ROUTE)
+						.authenticated()
+						.pathMatchers(API_ROUTE)
+						.authenticated()
+						.and()
+						.authorizeExchange()
+						.anyExchange()
+						.denyAll()
+						.and()
+						.csrf().disable()
+						.build();
+	}
+
+	/**
+	 * An {@link AuthenticationWebFilter} which authenticates the user with Http Basic authentication
+	 * and issues a token afterwards.
+	 *
+	 * @return basic to JWT AuthenticationWebFilter
+	 */
+	@Bean
+	private AuthenticationWebFilter basicToJwtAuthenticationFilter() {
+		AuthenticationWebFilter webFilter =
+						new AuthenticationWebFilter(new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository()));
+		webFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers(HttpMethod.POST, LOGIN_ROUTE));
+		webFilter.setAuthenticationSuccessHandler(new WebFilterChainServerJWTAuthenticationSuccessHandler(secret, expirationTime, issuer));
+		return webFilter;
+	}
+
+	/**
+	 * An {@link AuthenticationWebFilter} which validates the user's Authorization token
+	 * and gives access if the validation was successful.
+	 *
+	 * @return JWT AuthenticationWebFilter
+	 */
+	@Bean
+	private AuthenticationWebFilter jwtAuthenticationFilter() {
+		AuthenticationWebFilter webFilter = new AuthenticationWebFilter(new JWTReactiveAuthenticationManager());
+		webFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers(API_ROUTE));
+		webFilter.setAuthenticationConverter(new ServerHttpBearerAuthenticationConverter(secret));
+		return webFilter;
+	}
+}
