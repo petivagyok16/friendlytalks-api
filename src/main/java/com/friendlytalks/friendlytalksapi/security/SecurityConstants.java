9deaeef77390e9124f7bc1f7e044eae45b0aa4be
peter palocz <peter.palocz91@gmail.com>
2018-04-30 11:30:14 +0200

    refactor(SecurityConstants): refactor strings

--- src/main/java/com/friendlytalks/friendlytalksapi/security/SecurityConstants.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/security/SecurityConstants.java
@@ -3,11 +3,9 @@ package com.friendlytalks.friendlytalksapi.security;
 public class SecurityConstants {
 	public static final String SECRET = "SecretKeyToGenJWTs";
 	public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-	public static final String TOKEN_PREFIX = "Bearer ";
+	public static final String TOKEN_PREFIX = "Bearer";
 	public static final String TOKEN_HEADER = "Authorization";
 	public static final String ISSUER = "friendlytalksapi";
 	public static final String TOKEN_PARAM= "token";
-	public static final String SIGN_UP_URL = "/auth/signup";
-	public static final String SIGN_IN_URL = "/auth/signin";
 	public static final String API_ROUTE = "/api/**";
 }
