8c81797df641130ab79a829af72b9fd39938ed46
peter palocz <peter.palocz91@gmail.com>
2018-04-30 11:31:14 +0200

    feat(UserRepository): add new method

--- src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.friendlytalks.friendlytalksapi.repository;
 
 import com.friendlytalks.friendlytalksapi.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
 
-	Mono<User> findByUsername(String username);
+	Mono<UserDetails> findByUsername(String username);
+	Mono<User> findUserByUsername(String username);
 
 }
