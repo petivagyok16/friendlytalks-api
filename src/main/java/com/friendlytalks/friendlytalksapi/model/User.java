1b9ae321e43f81c1144c6a93381617836550b25a
Peter P <peter.palocz91@gmail.com>
2018-05-03 16:24:44 +0200

    feat(User): implement UserDetails interface

--- src/main/java/com/friendlytalks/friendlytalksapi/model/User.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/model/User.java
@@ -2,19 +2,26 @@ package com.friendlytalks.friendlytalksapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 @Document(collection = "users")
 @Data
 @Getter
 @Setter
-public class User {
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
 
 	@Id
 	private String id;
@@ -29,6 +36,8 @@ public class User {
 	private List<String> messages;
 	private Relations relations;
 	private Rating ratings;
+	private List<String> roles = new ArrayList<>();
+	private boolean enabled;
 
 	public User(
 					@JsonProperty("id") String id,
@@ -54,6 +63,46 @@ public class User {
 		this.messages = messages;
 		this.relations = relations;
 		this.ratings = ratings;
+
+		// TODO: authentication works currently with roles, so that all the users will get admin role, but we dont use roles in the client application
+		this.roles = Arrays.asList("ROLE_ADMIN");
+		this.enabled = true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList(roles.toArray(new String[roles.size()]));
+	}
+
+	//@JsonIgnore
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	//@JsonIgnore
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	//@JsonIgnore
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
 	}
 
 	@JsonIgnore
