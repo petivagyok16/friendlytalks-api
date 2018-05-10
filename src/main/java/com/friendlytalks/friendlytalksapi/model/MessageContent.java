60f7240a7657306a26a71afe4475e6fcb3af981b
Peter P <peter.palocz91@gmail.com>
2018-05-10 11:58:49 +0200

    feat(MessageContent): add validation constrains

--- src/main/java/com/friendlytalks/friendlytalksapi/model/MessageContent.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/model/MessageContent.java
@@ -2,11 +2,15 @@ package com.friendlytalks.friendlytalksapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * A wrapper class for editing existing messages.
  * The HttpRequestBody will only hold the new message content.
  */
 public class MessageContent {
+
+	@NotBlank
 	private String content;
 
 	public MessageContent(@JsonProperty("content") String content) {
