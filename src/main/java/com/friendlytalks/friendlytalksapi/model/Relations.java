de95f22ddf6efd838c634b4c2b5413ea6b38ba3b
Peter P <peter.palocz91@gmail.com>
2018-05-10 15:42:56 +0200

    refactor(Relations): refactor properties

--- src/main/java/com/friendlytalks/friendlytalksapi/model/Relations.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/model/Relations.java
@@ -3,21 +3,25 @@ package com.friendlytalks.friendlytalksapi.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 @Data
+@NoArgsConstructor
 public class Relations {
 
-	private List<String> followers;
-	private List<String> following;
+	private Set<String> followers = new HashSet<>();
+	private Set<String> following = new HashSet<>();
 
 	public Relations(
-					@JsonProperty("followers") List<String> followers,
-					@JsonProperty("following") List<String> following
+					@JsonProperty("followers") Set<String> followers,
+					@JsonProperty("following") Set<String> following
 	) {
 		this.followers = followers;
 		this.following = following;
