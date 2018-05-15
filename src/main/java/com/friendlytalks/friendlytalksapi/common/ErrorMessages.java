a6ef9ae1b2d21c3499d82288d650416eafc1ca3a
Peter P <peter.palocz91@gmail.com>
2018-05-15 12:24:39 +0200

    feat(ErrorMessages): add edit user not allowed

--- src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
@@ -7,4 +7,5 @@ public class ErrorMessages {
 	public static final String INVALID_TOKEN= "Invalid authentication token!";
 	public static final String MESSAGE_NOT_FOUND= "Message not found!";
 	public static final String MESSAGE_NOT_FOUND_AT_USER= "Message was deleted but the User who owned the message did not had the message for some reason.";
+	public static final String EDIT_USER_NOT_ALLOWED= "Editing the user is not allowed!";
 }
