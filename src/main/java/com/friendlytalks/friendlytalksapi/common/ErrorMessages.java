c2d758f8db243bb63cf8f485ab6fe1fd04ed405d
peter palocz <peter.palocz91@gmail.com>
2018-05-15 20:28:38 +0200

    feat(ErrorMessages): add new errormessage

--- src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
@@ -8,4 +8,5 @@ public class ErrorMessages {
 	public static final String MESSAGE_NOT_FOUND= "Message not found!";
 	public static final String MESSAGE_NOT_FOUND_AT_USER= "Message was deleted but the User who owned the message did not had the message for some reason.";
 	public static final String EDIT_USER_NOT_ALLOWED= "Editing the user is not allowed!";
+	public static final String INCONSISTENT_RATING= "Rating does not correspond with the Rating Cases!";
 }
