e5cf2bb945aa8f2782e1607a28764f28baa4bf41
Peter P <peter.palocz91@gmail.com>
2018-05-17 16:08:30 +0200

    refactor(ErrorMessages): refactor message

--- src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/common/ErrorMessages.java
@@ -9,5 +9,5 @@ public class ErrorMessages {
 	public static final String MESSAGE_NOT_FOUND_AT_USER = "Message was deleted but the User who owned the message did not had the message for some reason.";
 	public static final String EDIT_USER_NOT_ALLOWED = "Editing the user is not allowed!";
 	public static final String INCONSISTENT_RATING = "Rating does not correspond with the Rating Cases!";
-	public static final String DATABASE_ERROR = "Something went wrong during accessing the Database!";
+	public static final String DATABASE_ERROR = "Something went wrong during the process. Please try again later!";
 }
