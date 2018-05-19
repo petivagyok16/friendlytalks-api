d680ce85943565cddee4468b04c8bca34dae0ff0
peter palocz <peter.palocz91@gmail.com>
2018-05-20 00:41:37 +0200

    feat(MessageController): add preAuthorize

--- src/main/java/com/friendlytalks/friendlytalksapi/controller/MessageController.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/controller/MessageController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -32,6 +33,7 @@ public class MessageController {
 	}
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public Mono<HttpResponseWrapper<List<Message>>> getAllMessage() {
 		return this.messageService.getAllMessage();
 	}
