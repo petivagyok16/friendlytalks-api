725b498801f2a9d94818a910445e23355994d539
Peter P <peter.palocz91@gmail.com>
2018-05-23 12:41:58 +0200

    refactor(HttpResponseWrapper): delete token version

--- src/main/java/com/friendlytalks/friendlytalksapi/model/HttpResponseWrapper.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/model/HttpResponseWrapper.java
@@ -1,6 +1,5 @@
 package com.friendlytalks.friendlytalksapi.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,15 +10,7 @@ public class HttpResponseWrapper<T> implements Serializable {
 
 	private T payload;
 
-	@JsonInclude(JsonInclude.Include.NON_NULL)
-	private String token;
-
 	public HttpResponseWrapper(T payload) {
 		this.payload = payload;
-	}
-
-	public HttpResponseWrapper(T payload, String token) {
-		this.payload = payload;
-		this.token = token;
 	}
 }
