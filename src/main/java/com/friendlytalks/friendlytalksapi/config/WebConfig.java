4266421e985a181b22206017fb659897525c8327
Peter P <peter.palocz91@gmail.com>
2018-05-28 14:39:35 +0200

    refactor(WebConfig): refactor cors config

--- src/main/java/com/friendlytalks/friendlytalksapi/config/WebConfig.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebFluxConfigurer {
 	private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
 	private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
 	private static final String ALLOWED_ORIGIN = "*";
-	private static final String MAX_AGE = "3600";
+	private static final Long MAX_AGE = 3600L;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -26,7 +26,8 @@ public class WebConfig implements WebFluxConfigurer {
 		registry.addMapping("/api/**")
 						.allowedOrigins(ALLOWED_ORIGIN)
 						.allowedMethods(ALLOWED_METHODS)
-						.allowedHeaders(ALLOWED_HEADERS);
+						.allowedHeaders(ALLOWED_HEADERS)
+						.maxAge(MAX_AGE);
 	}
 
 }
