8e5ab90cae8c0f18e6a9ddbd71fa14a54a48e138
Peter P <peter.palocz91@gmail.com>
2018-05-28 16:20:13 +0200

    feat(WebConfig): add cors WebConfig

--- src/main/java/com/friendlytalks/friendlytalksapi/config/WebConfig.java
+++ src/main/java/com/friendlytalks/friendlytalksapi/config/WebConfig.java
@@ -10,8 +10,8 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
 
-	private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
-	private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+	private static final String ALLOWED_HEADERS = "Access-Control-Allow-Origin, Authorization, Access-Control-Allow-Credentials, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method";
+	private static final String ALLOWED_METHODS = "GET, PUT, POST, PATCH, DELETE, OPTIONS";
 	private static final String ALLOWED_ORIGIN = "*";
 	private static final Long MAX_AGE = 3600L;
 
